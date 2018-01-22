a989d7839805dfda675bb1a2eb8b20ba9a008102
Lars Kuehne <lars.kuehne@uni-jena.de>
2018-01-22 11:58:58 +0100

    formatting according to Google Style Guide, and added missing header

--- compute.cpp
+++ compute.cpp
@@ -1,7 +1,10 @@
 #include "compute.hpp"
 
+#include <algorithm>
+
 float compute(float alpha, float beta, aligned_vector<float> seed_x,
-          aligned_vector<float> seed_y, int num_iterations, float threshold) {
+              aligned_vector<float> seed_y, int num_iterations,
+              float threshold) {
   int num_seeds = seed_x.size();
   assert(seed_y.size() == num_seeds);
 
@@ -13,7 +16,7 @@ float compute(float alpha, float beta, aligned_vector<float> seed_x,
 
   float d = 0.0;
 
-  for (int i = 0; i < num_iterations && d < threshold ; i++) {
+  for (int i = 0; i < num_iterations && d < threshold; i++) {
 #pragma omp simd aligned(xp, yp : 64)
     for (int s = 0; s < num_seeds; s++) {
       yp[s] = yp[s] + beta * std::sin(TWO_PI * xp[s]);
