b171a144f42c167e2e8a8708c2ba72feaddee5a4
Lars Kuehne <lars.kuehne@uni-jena.de>
2018-01-22 13:35:25 +0100

    more accurate PI

--- compute.hpp
+++ compute.hpp
@@ -1,18 +1,19 @@
 #ifndef COMPUTE_H
 #define COMPUTE_H
 
+#include <boost/align/aligned_allocator.hpp>
 #include <cmath>
 #include <vector>
-#include <boost/align/aligned_allocator.hpp>
 
 template <typename T>
 using aligned_allocator = boost::alignment::aligned_allocator<T, 64>;
 template <typename T>
 using aligned_vector = std::vector<T, aligned_allocator<T>>;
 
-constexpr float PI = 3.141592635;
-constexpr float TWO_PI = 6.28318530718;  // 2 * PI;
+constexpr float PI = 3.14159265358979323846264338327950288419716939f;
+constexpr float TWO_PI = 2 * PI;
 
 float compute(float alpha, float beta, aligned_vector<float> seed_x,
-          aligned_vector<float> seed_y, int num_iterations, float threshold);
+              aligned_vector<float> seed_y, int num_iterations,
+              float threshold);
 #endif
