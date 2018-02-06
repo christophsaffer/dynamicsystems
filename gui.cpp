4cba97e6dc04c8a67468ceff5ad1e7de74d3e249
plgruener <pl.gruener@gmail.com>
2018-02-06 09:44:30 +0100

    subclass window, redrawing image works now

--- gui.cpp
+++ gui.cpp
@@ -2,11 +2,9 @@
 #include <vector>
 
 #include <FL/Fl.H>
-#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Window.H>
 #include <FL/Fl_Scroll.H>
 #include <FL/Fl_Box.H>
-#include <FL/fl_draw.H>
-#include <FL/Fl_Shared_Image.H>
 #include <FL/Fl_PNG_Image.H>
 
 #include <FL/Fl_Button.H>
@@ -17,34 +15,7 @@
 
 class ScrollBox : public Fl_Box {
     Fl_Scroll *scroll;
-    int window_x_1;
-    int window_y_1;
-    int param_x_1;
-    int param_y_1;
-    int window_x_2;
-    int window_y_2;
-    int param_x_2;
-    int param_y_2;
 public:
-    int handle(int e) {
-        if ( e == FL_PUSH ) {
-          // register mouse push as first corner of selection-rectangle
-          window_x_1 = Fl::event_x();
-          window_y_1 = Fl::event_y();
-          param_x_1 = Fl::event_x() - scroll->x() + scroll->hscrollbar.value();
-          param_y_1 = Fl::event_y() - scroll->y() + scroll->scrollbar.value();
-            fprintf(stderr, "event_x,event_y: %d,%d, Hit on box: %d,%d\n",
-              window_x_1, window_y_1, param_x_1, param_y_1);
-        } else if (e == FL_RELEASE) {
-          // register mouse release as second corner of selection-rectangle
-          window_x_2 = Fl::event_x();
-          window_y_2 = Fl::event_y();
-          param_x_2 = Fl::event_x() - scroll->x() + scroll->hscrollbar.value();
-          param_y_2 = Fl::event_y() - scroll->y() + scroll->scrollbar.value();
-          
-        }
-        return(Fl_Box::handle(e));
-    }
     void SetScroll(Fl_Scroll *val) {
         scroll = val;
     }
@@ -54,136 +25,145 @@ public:
     }
 };
 
-struct Params {
+class SimpleWindow : public Fl_Window {
 
-  Fl_Value_Input *in_num_iterations;
-  Fl_Value_Input *in_threshold;
-  Fl_Value_Input *in_alphamin;
-  Fl_Value_Input *in_alphamax;
-  Fl_Value_Input *in_alpha_num_intervals;
-  Fl_Value_Input *in_betamin;
-  Fl_Value_Input *in_betamax;
-  Fl_Value_Input *in_beta_num_intervals;
-  Fl_Value_Input *in_num_seedpoints;
-  Fl_Check_Button *in_output_csv;
+  public: 
+    SimpleWindow(int w, int h, const char* title);
+    ~SimpleWindow();
 
-  ScrollBox *box;
-  Fl_PNG_Image *picture;
+    Fl_Scroll* scroll;
+    ScrollBox* scrollbox;
 
-  int num_iterations;
-  float threshold;
-  float alphamin;
-  float alphamax;
-  int alpha_num_intervals;
-  float betamin;
-  float betamax;
-  int beta_num_intervals;
-  int num_seedpoints;
-  std::vector<float> seedpoints = {};
-  bool output_csv;
+    Fl_PNG_Image* image;
+
+    Fl_Button* button_compute;
+
+    Fl_Value_Input* in_num_iterations;
+    Fl_Value_Input* in_threshold;
+    Fl_Value_Input* in_alphamin;
+    Fl_Value_Input* in_alphamax;
+    Fl_Value_Input* in_alpha_num_intervals;
+    Fl_Value_Input* in_betamin;
+    Fl_Value_Input* in_betamax;
+    Fl_Value_Input* in_beta_num_intervals;
+    Fl_Value_Input* in_num_seedpoints;
+    Fl_Check_Button* in_output_csv;
+
+  private:
+    static void callback_compute(Fl_Widget*, void*);
+    inline void callback_compute_il();
+
 };
 
-void buttonPushed(Fl_Widget*, void* data) {
-  std::cout << "Button pushed. Do some work now!" << std::endl;
-
-  // save parameters from input boxes into variables
-  Params *params = reinterpret_cast<Params*>(data);
-  params->num_iterations = params->in_num_iterations->value();
-  params->threshold = params->in_threshold->value();
-
-  params->alphamin = params->in_alphamin->value();
-  params->alphamax = params->in_alphamax->value();
-  params->alpha_num_intervals = params->in_alpha_num_intervals->value();
-
-  params->betamin = params->in_betamin->value();
-  params->betamax = params->in_betamax->value();
-  params->beta_num_intervals = params->in_beta_num_intervals->value();
-
-  params->num_seedpoints = params->in_num_seedpoints->value();
-//  params->seedpoints = new std::vector<float>(0);
-  params->output_csv = params->in_output_csv->value();
-
-  // call compute function with parameters
-  std::cout << "amin:" << params->alphamin << std::endl;
-  std::cout << "amax:" << params->alphamax << std::endl;
-  std::cout << "csv?:" << params->output_csv << std::endl;
-
-  compute_all(
-   params->num_iterations,
-   params->threshold,
-   params->alphamin,
-   params->alphamax,
-   params->alpha_num_intervals,
-   params->betamin,
-   params->betamax,
-   params->beta_num_intervals,
-   params->num_seedpoints,
-   params->output_csv,
-   params->seedpoints
-  );
-  // redraw image after computation
-//  params->picture->draw();
-  params->box->image(params->picture);
+int main() {
+  SimpleWindow win(800,600,"Dynamic Systems");
+  return Fl::run();
 }
 
-int main (int argc, char **argv) {
-  Params input;
+// Constructor
+SimpleWindow::SimpleWindow(int w, int h, const char* title):Fl_Window(w,h,title) {
+  // between begin...end comes what to show in window
+  this->begin(); // this-> is implicit
+  
+  // Top: ScrollBox containing the picture
+  image = new Fl_PNG_Image("picture.png");
 
-  fl_register_images();
-  Fl_PNG_Image picture("picture.png");
-  Fl_Double_Window window(800,600,"Dynamic Systems");
-  Fl_Scroll scroll(0,0,800,400);
-  ScrollBox box(0,0,picture.w(),picture.h());
-	box.SetScroll(&scroll);
-  box.image(picture);
-	scroll.end();
-  // Parameter input
+  scroll = new Fl_Scroll(0,0,800,400);
+  scroll->begin();
+  scrollbox = new ScrollBox(0,0,800,400);
+  scrollbox->SetScroll(scroll);
+  scrollbox->image(image);
+
+  scroll->end();
+
+  // Bottom: inputboxes and button
+
   int boxwidth = 80;  // width input boxes
   int boxheight = 20;  // height input boxes
   int firstrow = 450;
-  int secondrow = 500;
+  int secondrow = 550;
   int padding = 100;
 
-  input.in_alphamin = new Fl_Value_Input(0*padding,firstrow,boxwidth,boxheight,"alpha_min");
-  input.in_alphamin->align(FL_ALIGN_TOP);
-  input.in_alphamin->value(0.0); 
-  input.in_alphamax = new Fl_Value_Input(1*padding,firstrow,boxwidth,boxheight,"alpha_max");
-  input.in_alphamax->align(FL_ALIGN_TOP);
-  input.in_alphamax->value(1.0); 
-  input.in_alpha_num_intervals = new Fl_Value_Input(2*padding,firstrow,boxwidth,boxheight,"width");
-  input.in_alpha_num_intervals->align(FL_ALIGN_TOP);
-  input.in_alpha_num_intervals->value(400); 
+  in_alphamin = new Fl_Value_Input(0*padding,firstrow,boxwidth,boxheight,"alpha_min");
+  in_alphamin->align(FL_ALIGN_TOP);
+  in_alphamin->value(0.0); 
 
-  input.in_betamin = new Fl_Value_Input(0*padding,secondrow,boxwidth,boxheight,"beta_min");
-  input.in_betamin->align(FL_ALIGN_TOP);
-  input.in_betamin->value(0.0); 
-  input.in_betamax = new Fl_Value_Input(1*padding,secondrow,boxwidth,boxheight,"beta_max");
-  input.in_betamax->align(FL_ALIGN_TOP);
-  input.in_betamax->value(1.0); 
-  input.in_beta_num_intervals = new Fl_Value_Input(2*padding,secondrow,boxwidth,boxheight,"height");
-  input.in_beta_num_intervals->align(FL_ALIGN_TOP);
-  input.in_beta_num_intervals->value(400); 
+  in_alphamax = new Fl_Value_Input(1*padding,firstrow,boxwidth,boxheight,"alpha_max");
+  in_alphamax->align(FL_ALIGN_TOP);
+  in_alphamax->value(1.0); 
 
-  input.in_num_iterations = new Fl_Value_Input(3*padding,firstrow,boxwidth,boxheight,"iterations");
-  input.in_num_iterations->align(FL_ALIGN_TOP);
-  input.in_num_iterations->value(100);
-  input.in_threshold = new Fl_Value_Input(4*padding,firstrow,boxwidth,boxheight,"threshold");
-  input.in_threshold->align(FL_ALIGN_TOP);
-  input.in_threshold->value(1);
+  in_alpha_num_intervals = new Fl_Value_Input(2*padding,firstrow,boxwidth,boxheight,"width");
+  in_alpha_num_intervals->align(FL_ALIGN_TOP);
+  in_alpha_num_intervals->value(400); 
 
-  input.in_num_seedpoints = new Fl_Value_Input(3*padding,secondrow,boxwidth,boxheight,"#seedpoints");
-  input.in_num_seedpoints->align(FL_ALIGN_TOP);
-  input.in_num_seedpoints->value(8);
+  in_betamin = new Fl_Value_Input(0*padding,secondrow,boxwidth,boxheight,"beta_min");
+  in_betamin->align(FL_ALIGN_TOP);
+  in_betamin->value(0.0); 
 
-  // Button
-  Fl_Button *button = new Fl_Button(5*padding,firstrow,boxwidth,boxheight,"compute");
-  button->type(FL_NORMAL_BUTTON);
-  button->color(FL_RED);
-  button->callback(buttonPushed,&input);
+  in_betamax = new Fl_Value_Input(1*padding,secondrow,boxwidth,boxheight,"beta_max");
+  in_betamax->align(FL_ALIGN_TOP);
+  in_betamax->value(1.0); 
 
-  input.in_output_csv = new Fl_Check_Button(5*padding,secondrow,boxwidth,boxheight,"csv output");
+  in_beta_num_intervals = new Fl_Value_Input(2*padding,secondrow,boxwidth,boxheight,"height");
+  in_beta_num_intervals->align(FL_ALIGN_TOP);
+  in_beta_num_intervals->value(400); 
 
-  window.resizable(window);
-  window.show();
-  return Fl::run();
+  in_num_iterations = new Fl_Value_Input(3*padding,firstrow,boxwidth,boxheight,"iterations");
+  in_num_iterations->align(FL_ALIGN_TOP);
+  in_num_iterations->value(100);
+
+  in_threshold = new Fl_Value_Input(4*padding,firstrow,boxwidth,boxheight,"threshold");
+  in_threshold->align(FL_ALIGN_TOP);
+  in_threshold->value(1);
+
+  in_num_seedpoints = new Fl_Value_Input(3*padding,secondrow,boxwidth,boxheight,"#seedpoints");
+  in_num_seedpoints->align(FL_ALIGN_TOP);
+  in_num_seedpoints->value(8);
+
+  button_compute = new Fl_Button(5*padding,firstrow,boxwidth,boxheight,"compute");
+  button_compute->type(FL_NORMAL_BUTTON);
+  button_compute->color(FL_RED);
+  button_compute->callback(callback_compute,this);
+
+  in_output_csv = new Fl_Check_Button(5*padding,secondrow,boxwidth,boxheight,"csv output");
+
+  this->end();
+  resizable(this);
+  this->show();
 }
+
+// Destructor
+SimpleWindow::~SimpleWindow() {}
+
+// Button callback, just cast object and call real function
+void SimpleWindow::callback_compute(Fl_Widget* o, void* v) {
+  ( (SimpleWindow*)v )->callback_compute_il();
+}
+
+// no arguments needed, because has access to all class members
+void SimpleWindow::callback_compute_il() {
+  std::cout << "start computation..." << std::endl;
+
+  std::vector<float> seedpoints = {};
+
+  compute_all(
+    in_num_iterations->value(),
+    in_threshold->value(),
+    in_alphamin->value(),
+    in_alphamax->value(),
+    in_alpha_num_intervals->value(),
+    in_betamin->value(),
+    in_betamax->value(),
+    in_beta_num_intervals->value(),
+    in_num_seedpoints->value(),
+    in_output_csv->value(),
+    seedpoints
+  );
+
+  image = new Fl_PNG_Image("picture.png");
+  scrollbox->image(image);
+  scrollbox->resize(0,0,image->w(),image->h());
+  redraw();
+  std::cout << "done." << std::endl;
+}
+
